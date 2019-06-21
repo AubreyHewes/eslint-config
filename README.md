1810011a71c7f1bb00323baf1aa598d469bcec1c
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-06-21 15:47:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# eslint-config
+
+This is a basic eslint config used by me.
+
+To install and use do the following:
+
+```
+yarn add --dev @hewes/eslint-config eslint prettier
+```
+
+And configure eslint to extend `@hewes/eslint-config`
+
+```json
+{
+  "extends": "@hewes/eslint-config"
+}
+```
+
+That should do it!
---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ["react", "prettier"],
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    "prettier/prettier": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-destructuring": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    "prefer-arrow-callback": "error",
+    "arrow-body-style": "error",
+    "prefer-promise-reject-errors": "error",
+    "prefer-rest-params": "error"
+  }
+};
